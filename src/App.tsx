b8be6b6b7e2f842625fa67bd926193dfbe7f49e2
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2025-11-27 21:22:35 +0530

    :zap: :art: added increment count on each second functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  // increment count on each second
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCount((prevCount) => prevCount + 1);
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <div>
+      <h1>Counter Application</h1>
+
+      <p>Count: {count}</p>
+    </div>
+  );
 }
 
 export default App;
